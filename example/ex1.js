4e156fa252325ab6658946e4ef0721f000efad90
Lenfer <lenfer.spb@gmail.com>
2013-11-13 13:16:27 +0400

    Add simple example

---
+++ example/ex1.js
@@ -0,0 +1,59 @@
+﻿
+var wsdlService = require('wsdl-service')
+
+
+// Define SOAP service
+var SOAP = new wsdlService({
+	name: 'shedule-service', 
+	url: 'http://localhost/test-service/'
+})
+
+// =================================================
+// getStartTime
+SOAP.element({
+    name: 'getStartTimeOut', 
+    sequence:{
+        startTime: {type: 'dateTime'}
+    }
+})
+SOAP.port({
+    name: 'getStartTime', 
+    info: 'Возвращает время запуска сервиса', 
+    output: 'getStartTimeOut', 
+    exec: function(req, next){
+        next(false, {startTime: Schedule.stats().startTime})
+    }
+})
+
+
+// =================================================
+// getIgnoreScheduleList
+SOAP.complexType({
+    name: 'getIgnoreScheduleListOutElement', 
+    sequence:{
+        idShedule: {type: 'integer'}, 
+        idToch: {type: 'integer'}, 
+        nameShedule: {type: 'string'}, 
+        time: {type: 'string'}       
+    }
+})
+SOAP.element({
+    name: 'getIgnoreScheduleListOut', 
+    sequence:{
+        getIgnoreScheduleListOutElement: {
+            type: 'getIgnoreScheduleListOutElement', 
+            minOccurs: 0,
+            maxOccurs: 'unbounded'
+        }
+    }
+})
+SOAP.port({
+    name: 'getIgnoreScheduleList', 
+    info: 'Возвращает список проигнорированных заданий по расписанию, за текущий день', 
+    output: 'getIgnoreScheduleListOut', 
+    exec: function(req, next){
+        next(false, {
+            getIgnoreScheduleListOutElement: Schedule.ignoredSсheduleList()            
+        })
+    }
+})
