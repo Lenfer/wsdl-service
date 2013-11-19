1e0b7a0b420cea464c098cce2aefacdab2ffbed6
Lenfer <lenfer.spb@gmail.com>
2013-11-19 20:23:51 +0400

    Fix for VFP

---
+++ test/VPF_answer_parse.js
@@ -0,0 +1,19 @@
+﻿var expatParser = require('xml2json')
+	, inspect = require('eyes').inspector({maxLength: 204800})
+
+
+// Load test xml
+var xml = require('fs').readFileSync('./VFP_answer.xml').toString('utf8')
+
+console.log(xml)
+
+console.log('==================================================================')
+console.log('==================================================================')
+console.log('==================================================================')
+
+var parsed = expatParser.toJson(xml, {
+	object: true, 
+	coerce: false 
+})
+
+inspect(parsed)
